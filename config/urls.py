0f3b49a78c3275e99db9aa3c81c916c710c65840
Tomar1908 <aman.tomar@digimonk.in>
2025-12-19 21:49:59 +0530

    Remove caddy_ask view and update URL routing; add NotificationAdmin to admin panel

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from config.views import caddy_ask
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Loyalty API",
@@ -43,7 +43,6 @@ urlpatterns = [
     path('', include('super_admin.urls')),
     path('admin/', admin.site.urls),
     path("api/", include("config.apis", namespace="api")),
-    path("caddy/ask", caddy_ask),
 
 ]
 
