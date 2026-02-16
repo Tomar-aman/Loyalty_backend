b698d3c6f5d21b281c226ec1de8d455d62e84dcb
Tomar1908 <aman.tomar@digimonk.in>
2026-02-16 16:46:30 +0530

    feat: Implement search functionality in NewsArticleListView

--- news/views.py
+++ news/views.py
@@ -5,11 +5,28 @@ from .models import NewsArticle
 from .serializers import NewsArticleSerializer
 from rest_framework.permissions import AllowAny
 from django.db import DatabaseError
+from django.db.models import Q
 
 class NewsArticleListView(GenericAPIView):
     permission_classes = [AllowAny]
-    queryset = NewsArticle.objects.all().order_by('-published_at')
     serializer_class = NewsArticleSerializer
+
+    def get_queryset(self):
+        """
+        Override get_queryset to apply search filters
+        """
+        queryset = NewsArticle.objects.all().order_by('-published_at')
+        
+        # Get search parameter from query params
+        search = self.request.query_params.get('search', None)
+        
+        if search:
+            # Search in title and content fields (case-insensitive)
+            queryset = queryset.filter(
+                Q(title__icontains=search)  #| Q(content__icontains=search)
+            )
+        
+        return queryset
 
     def get(self, request, *args, **kwargs):
         try:
